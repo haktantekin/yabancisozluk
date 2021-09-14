44d0a2edd9ae7073694fb26cd27adee822d9746f
haktantekin <47484364+haktantekin@users.noreply.github.com>
2021-09-14 23:57:15 +0300

    created project

---
+++ html/assets/scripts/natkah.js
@@ -0,0 +1,11 @@
+
+
+WebFont.load({
+    google: {
+        families: ['https://fonts.googleapis.com/css2?family=Passion+One&family=Poppins:wght@300;400;500&display=swap&subset=latin-ext']
+    }
+});
+
+// Analytics 
+window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config','G-67DGC1MGCF');
+// Analytics
